670aede826737d7244ed764608fb4a8d5d33d8c4
Jason Andrada <jpa323@nyu.edu>
2017-07-03 16:31:17 -0400

    all data rendering

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -29,13 +29,54 @@ $('.test-form').submit((e) =>{
       },
       success: data =>{
         console.log('got this back from form ajax', data)
+        renderGem(data.response_data, data.dependencies)
       },
       error: error =>{
         console.log('form ajax error: ', error)
       }
     })
-
   })
+
+  const renderGem = (gem, array) =>{
+    //holder div for a new gem
+    let result = $('<div>', {
+      class: 'search-result'
+    });
+    let results = $('#search-results')
+    //append holder div to the hardcoded HTML
+    result.appendTo(results)
+    //card-style div to display information about a gem
+    let newGem = $('<div>', {
+      class: 'gem'
+    });
+    newGem.appendTo(result);
+    //add the gem's name (and link to rubygems.org info) to the DOM
+    let gemName = $('<a>', {
+      class: 'gem-name',
+      href: `https://rubygems.org/gems/${gem.name}`
+    });
+    gemName.text(gem.name);
+    gemName.appendTo(newGem);
+
+    //add gem's information to the DOM
+    let gemInfo = $('<div>', {
+      class: 'gem-info'
+    });
+    gemInfo.text(gem.info);
+    gemInfo.appendTo(newGem);
+
+    let dependencies = $('<div>', {
+      class: 'dependencies'
+    }).appendTo(newGem)
+
+    array.map((dependency) =>{
+      return $('<div>', {
+        class: 'dependency'
+      }).text(dependency).appendTo(dependencies)
+    })
+  }
+
+
 
   //add a click listener for the button that will save to localStorage
   //localStorage.setItem('gem', 'the new gem')
