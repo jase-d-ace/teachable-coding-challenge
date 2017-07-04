684afe4815a24ee7cecf3359012ba9375e280413
Jason Andrada <jpa323@nyu.edu>
2017-07-04 07:51:09 -0400

    deleting from favorites page

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -6,11 +6,24 @@ $(document).ready(() =>{
 
   let storageKeys = Object.keys(myStorage);
   let faves = $('#favorites-list')
-  let allFaves = storageKeys.map((key) =>{
-    return $('<a>', {
-      href: `https://rubygems.org/gems/${myStorage[key]}`
-    }).text(key).appendTo(faves)
-  })
+  // let allFaves = storageKeys.map((key) =>{
+  //   return $('<a>', {
+  //     href: `https://rubygems.org/gems/${myStorage[key]}`,
+  //     class: 'favorite'
+  //   }).text(key).appendTo(faves)
+// })
+    storageKeys.forEach(key =>{
+      let dependencyLink = $('<a>', {
+        class: 'favorite',
+        href: `https://rubygems.org/gems/${myStorage[key]}`
+      }).text(key).appendTo(faves);
+      let deleteButton = $('<button>', {
+        class: 'delete-button'
+      }).click(() =>{
+        myStorage.removeItem(key);
+        window.location.reload();
+      }).text('Delete').appendTo(faves)
+    })
 
 
 })//end of document ready
